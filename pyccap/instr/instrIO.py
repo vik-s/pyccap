facba328786ea989d471016349db8ef4b2b77443
Sekar <Vikram.Sekar@idt.com>
2018-04-07 09:03:13 -0700

    added returns to query methods, added initialize method

--- pyccap/instr/instrIO.py
+++ pyccap/instr/instrIO.py
@@ -3,6 +3,12 @@ Module to handle GPIB IO commands
 """
 import pyvisa
 
+def initialize(self, visa_name):
+    rm = pyvisa.ResourceManager()
+    self.pyvisa = rm.open_resource(visa_name)
+    self.pyvisa.timeout = None
+    self.pyvisa.read_termination = '\n'
+
 def write(self, message):
     """
     Write into instrument
@@ -15,22 +21,22 @@ def query_asc(self, message):
     """
     Query ASCII values
     """
-    self.pyvisa.query_ascii_values(message)
+    return self.pyvisa.query_ascii_values(message)
 
 def query_bin(self, message):
     """
     Query binary values
     """
-    self.pyvisa.query_ascii_values(message)
+    return self.pyvisa.query_ascii_values(message)
 
 def query(self, message):
     """
     Query values
     """
-    self.pyvisa.query(message)
+    return self.pyvisa.query(message)
 
 def read(self, message):
     """
     Read values
     """
-    self.pyvisa.query(message)
+    return self.pyvisa.query(message)
